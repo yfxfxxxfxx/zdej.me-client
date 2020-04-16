ef0fcb361b9b19203f47e2e425ac33864945e8a2
yfxfxxxfxx <mail@przykladowy.co.pl>
2020-04-16 10:54:28 +0200

    Implemented root GalleryComponent, updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {UploadComponent} from './upload/upload.component';
+import {GalleryComponent} from './gallery/gallery.component';
 
 
 const routes: Routes = [
   { path: 'home', component: UploadComponent },
-  // { path: 'gallery', component: GalleryComponent },
+  { path: 'gallery', component: GalleryComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpImageService} from '../services/http-image.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  thumbnailURLs: string[] = [];
+
+  constructor(
+    private httpImageService: HttpImageService,
+    private http: HttpClient
+  ) {
+    this.httpImageService = new HttpImageService(http);
+  }
 
   ngOnInit(): void {
+    this.getImagesFromServer();
+  }
+
+  getImagesFromServer() {
+    this.httpImageService.getAllImages().subscribe(
+      (data) => {
+        for (const url of data) {
+          this.thumbnailURLs.push(url);
+        }
+      },
+      error => console.log(error),
+      () => console.log('OK')
+    );
   }
 
 }
