acfc9c7f454956013b09b2da16e6a656c857ad21
yfxfxxxfxx <mail@przykladowy.co.pl>
2020-04-16 09:30:13 +0200

    Created Conversion enum, implemented AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {Conversion} from './enum/conversion.enum';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  colorMenu = false;
+  conversion: Conversion;
+  @Output() onConversionChange: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {
+    this.conversion = 1;
+  }
+
+  updateConversionType(conversion) {
+    console.log('conversion argument: ' + conversion);
+    this.conversion = conversion;
+    this.onConversionChange.emit({ conversion: conversion });
+  }
+
+  selectComponent() {
+    return 'selected-option-tag';
+  }
+
+  deselectComponent() {
+    return 'option-tag';
+  }
+
+  selectSidebarComponent() {
+    return 'selected-sidebar-option';
+  }
+
+  deselectSidebarComponent() {
+    return 'sidebar-option';
+  }
+
+  showColorMenu() {
+    this.colorMenu = !this.colorMenu;
+  }
 }
---
+++ src/app/enum/conversion.enum.ts
@@ -0,0 +1,8 @@
+export enum Conversion {
+  ASCII = 0,
+  RGB0 = 1,
+  RGB1 = 2,
+  RGB2 = 3,
+  GRAY = 4,
+  NEG = 5
+}
